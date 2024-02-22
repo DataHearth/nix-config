50d6b732668dcc78e6b6b1c5317832bdaafd9937
DataHearth <dev@antoine-langlois.net>
2024-02-22 11:46:35 +0100

    increase gpg-agent default cache

--- modules/home-manager/services/gpg_ssh_agent.nix
+++ modules/home-manager/services/gpg_ssh_agent.nix
@@ -5,6 +5,8 @@
       enable = true;
       enableZshIntegration = true;
       pinentryFlavor = "qt";
+      defaultCacheTtl = 86400;
+      maxCacheTtl = 86400;
     };
     
     ssh-agent.enable = true;
