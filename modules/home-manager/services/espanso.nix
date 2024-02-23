4780da367364ab2f6445814b5270b48759f32d6b
DataHearth <dev@antoine-langlois.net>
2024-02-23 11:30:36 +0100

    add espanso

--- modules/home-manager/services/espanso.nix
+++ modules/home-manager/services/espanso.nix
@@ -4,41 +4,43 @@
     enable = true;
     # configs = {};
     matches = {
-      default = [
-        {
-          trigger = ":date";
-          replace = "{date}";
-          vars = [
-            {
-              name = "date";
-              type = "date";
-              format = "%d/%m/%Y";
-            }
-          ];
-        }
-        {
-          trigger = ":datetime";
-          replace = "{datetime}";
-          vars = [
-            {
-              name = "datetime";
-              type = "date";
-              format = "%d/%m/%Y %H:%M:%S";
-            }
-          ];
-        }
-        {
-          trigger = ":time";
-          replace = "{time}";
-          vars = [
-            {
-              name = "time";
-              type = "date";
-              format = "%H:%M:%S";
-            }
-          ];
-        }
-      ];
+      default = {
+        matches = [
+          {
+            trigger = ":date";
+            replace = "{date}";
+            vars = [
+              {
+                name = "date";
+                type = "date";
+                format = "%d/%m/%Y";
+              }
+            ];
+          }
+          {
+            trigger = ":datetime";
+            replace = "{datetime}";
+            vars = [
+              {
+                name = "datetime";
+                type = "date";
+                format = "%d/%m/%Y %H:%M:%S";
+              }
+            ];
+          }
+          {
+            trigger = ":time";
+            replace = "{time}";
+            vars = [
+              {
+                name = "time";
+                type = "date";
+                format = "%H:%M:%S";
+              }
+            ];
+          }
+        ];
+      };
     };
   };
 }
