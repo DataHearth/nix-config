1af6cc59166a65f83c8ca58333e903d630eb74e2
DataHearth <dev@antoine-langlois.net>
2024-06-13 10:29:52 +0200

    configuration(nixos): enable tlp

--- hosts/antoine-nixos/services.nix
+++ hosts/antoine-nixos/services.nix
@@ -1,6 +1,16 @@
 { ... }: {
   services = {
     blueman.enable = false;
+    tlp = {
+      enable = true;
+      settings = {
+        TLP_DEFAULT_MODE = "AC";
+        TLP_PERSISTENT_DEFAULT = 1;
+        CPU_DRIVER_OPMODE_ON_AC = "active";
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      };
+    };
 
     # Custom services
     passthrough = {
