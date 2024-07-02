a292528c32e4166319656261e8049b8c1bec94df
DataHearth <dev@antoine-langlois.net>
2024-07-02 11:35:46 +0200

    modules(neovim): update harpoon keymaps

--- modules/neovim/keymaps/harpoon.nix
+++ modules/neovim/keymaps/harpoon.nix
@@ -19,21 +19,31 @@
       end
     '';
     key = "<leader>e";
+    options.desc = "Open harpoon floating window with telescope";
   }
   {
-    action.__raw = "function() require('harpoon'):list():append() end";
+    action.__raw = "function() require('harpoon'):list():add() end";
     key = "<leader>ha";
+    options.desc = "Harpoon - Add current file";
+  }
+  {
+    action.__raw = "function() require('harpoon'):list():remove() end";
+    key = "<leader>hr";
+    options.desc = "Harpoon - Remove current file";
   }
   {
     action.__raw = "function() require('harpoon'):list():prev() end";
     key = "<leader>hp";
+    options.desc = "Harpoon - Go to previous file";
   }
   {
     action.__raw = "function() require('harpoon'):list():next() end";
     key = "<leader>hn";
+    options.desc = "Harpoon - Go to next file";
   }
   {
     action.__raw = "function() require('harpoon'):list():clear() end";
     key = "<leader>hc";
+    options.desc = "Harpoon - Clear list";
   }
 ]
