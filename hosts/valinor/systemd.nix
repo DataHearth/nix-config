9451fea0dac23339514fbb64c8c03b0b7a195ad0
DataHearth <dev@antoine-langlois.net>
2025-03-21 21:32:17 +0100

    configurations(valinor): add 2nd pgsql database backup

--- hosts/valinor/systemd.nix
+++ hosts/valinor/systemd.nix
@@ -35,6 +35,7 @@
         };
         backups_secrets = "/run/secrets/backups";
         pgsql16_bak = "/tmp/pgsql16-backup.sql";
+        pgsql17_bak = "/tmp/pgsql17-backup.sql";
       in
       {
         NetworkManager-wait-online.enable = false;
@@ -52,11 +53,12 @@
             } // environment;
             script = ''
               docker compose -f /mnt/Erebor/War-goats/appdata/docker-compose.yml exec -it postgresql16 pg_dumpall -U postgres > ${pgsql16_bak}
+              docker compose -f /mnt/Erebor/War-goats/appdata/docker-compose.yml exec -it postgresql17 pg_dumpall -U postgres > ${pgsql17_bak}
 
               restic backup \
                 --files-from ${storj_secrets}/include.txt \
                 --exclude-file ${storj_secrets}/exclude.txt \
-                ${pgsql16_bak}
+                ${pgsql16_bak} ${pgsql17_bak}
 
               restic forget --keep-last 2 --prune
 
@@ -77,11 +79,12 @@
             } // environment;
             script = ''
               docker compose -f /mnt/Erebor/War-goats/appdata/docker-compose.yml exec -it postgresql16 pg_dumpall -U postgres > ${pgsql16_bak}
+              docker compose -f /mnt/Erebor/War-goats/appdata/docker-compose.yml exec -it postgresql17 pg_dumpall -U postgres > ${pgsql17_bak}
 
               restic backup \
                 --files-from ${gondoline_secrets}/include.txt \
                 --exclude-file ${gondoline_secrets}/exclude.txt \
-                ${pgsql16_bak}
+                ${pgsql16_bak} ${pgsql17_bak}
 
               restic forget --keep-last 2 --prune
 
