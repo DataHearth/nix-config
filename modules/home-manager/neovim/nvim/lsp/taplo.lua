77117c30e4dd8d6d75c0518a301e3dea90ea0b42
DataHearth <dev@antoine-langlois.net>
2026-01-10 10:04:23 +0100

    feat(neovim): add taplo LSP settings for config file support

--- modules/home-manager/neovim/nvim/lsp/taplo.lua
+++ modules/home-manager/neovim/nvim/lsp/taplo.lua
@@ -1,5 +1,14 @@
 return {
   cmd = { 'taplo', 'lsp', 'stdio' },
   filetypes = { 'toml' },
-  root_markers = { '*.toml' },
+  root_markers = { '*.toml', 'taplo.toml', '.taplo.toml' },
+  settings = {
+    evenBetterToml = {
+      taplo = {
+        configFile = {
+          enabled = true,
+        },
+      },
+    },
+  },
 }
