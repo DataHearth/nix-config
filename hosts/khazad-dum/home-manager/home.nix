aaa7e00a443599e0c0eecfd6a93e88eed2743101
DataHearth <dev@antoine-langlois.net>
2026-02-16 02:40:21 +0100

    feat: set Zen Browser as default browser via xdg mimeApps

--- hosts/khazad-dum/home-manager/home.nix
+++ hosts/khazad-dum/home-manager/home.nix
@@ -29,5 +29,24 @@
   xdg = {
     enable = true;
     autostart.enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "x-scheme-handler/sgnl" = "signal.desktop";
+        "x-scheme-handler/signalcaptcha" = "signal.desktop";
+        "x-scheme-handler/http" = "zen-beta.desktop";
+        "x-scheme-handler/https" = "zen-beta.desktop";
+        "x-scheme-handler/chrome" = "zen-beta.desktop";
+
+        "text/html" = "zen-beta.desktop";
+
+        "application/x-extension-htm" = "zen-beta.desktop";
+        "application/x-extension-html" = "zen-beta.desktop";
+        "application/x-extension-shtml" = "zen-beta.desktop";
+        "application/xhtml+xml" = "zen-beta.desktop";
+        "application/x-extension-xhtml" = "zen-beta.desktop";
+        "application/x-extension-xht" = "zen-beta.desktop";
+      };
+    };
   };
 }
